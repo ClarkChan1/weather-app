5e4fa56f9c56494ca66253dddba8181bee70595a
ClarkChan1 <clark2chan@gmail.com>
2021-08-16 10:59:11 -0700

    added weath info

--- src/index.js
+++ src/index.js
@@ -42,6 +42,55 @@ function createUI() {
   searchContainer.addEventListener('submit', handleSearch);
 }
 
+function makeBody(weatherInfo) {
+  // make the container that will display all the weather information
+  const infoContainer = document.createElement('div');
+  infoContainer.classList.add('info-container');
+  // make the contents of the info container
+  let nameAndTempContainer = document.createElement('div');
+  nameAndTempContainer.classList.add('name-and-temp-container');
+  let locationName = document.createElement('p');
+  locationName.classList.add('location-name');
+  locationName.appendChild(document.createTextNode(weatherInfo.locationName));
+  let separator = document.createElement('p');
+  separator.appendChild(document.createTextNode('|'));
+  separator.classList.add('separator');
+  let tempContainer = document.createElement('div');
+  tempContainer.classList.add('temp-container');
+  let currentTemp = document.createElement('p');
+  currentTemp.appendChild(document.createTextNode(weatherInfo.fahrenheitTemp));
+  currentTemp.classList.add('current-temp');
+  let lowestTemp = document.createElement('p');
+  lowestTemp.appendChild(document.createTextNode(weatherInfo.fahrenheitLowest));
+  lowestTemp.classList.add('other-temps');
+  let highestTemp = document.createElement('p');
+  highestTemp.appendChild(document.createTextNode(weatherInfo.fahrenheitHighest));
+  highestTemp.classList.add('other-temps');
+  tempContainer.appendChild(lowestTemp);
+  tempContainer.appendChild(currentTemp);
+  tempContainer.appendChild(highestTemp);
+  nameAndTempContainer.appendChild(locationName);
+  nameAndTempContainer.appendChild(separator);
+  nameAndTempContainer.appendChild(tempContainer);
+
+  let lineBreak = document.createElement('hr');
+  lineBreak.style.width = '100%';
+
+  let description = document.createElement('p');
+  description.appendChild(document.createTextNode(weatherInfo.description));
+  description.classList.add('description');
+  // append all the info to infoContainer
+  infoContainer.appendChild(nameAndTempContainer);
+  infoContainer.appendChild(lineBreak);
+  infoContainer.appendChild(description);
+  // remove previous info container if it exists
+  if (wrapper.querySelector('.info-container') != null) {
+    wrapper.removeChild(wrapper.querySelector('.info-container'));
+  }
+  // append the new information container to the wrapper
+  wrapper.appendChild(infoContainer);
+}
+
 async function handleSearch(e) {
   e.preventDefault();
 
@@ -50,22 +99,10 @@ async function handleSearch(e) {
   // clear the searchBox
   searchBox.value = "";
   let weatherInfo = await getWeatherInfo(userInput);
-  // make the container that will display all the weather information
-  const infoContainer = document.createElement('div');
-  infoContainer.classList.add('info-container');
-  // make the contents of the info container
-  let locationName = document.createElement('p');
-  locationName.classList.add('location-name');
-  locationName.appendChild(document.createTextNode(weatherInfo.locationName));
 
-  // append all the info to infoContainer
-  infoContainer.appendChild(locationName);
-  // remove previous info container if it exists
-  if (wrapper.querySelector('.info-container') != null) {
-    wrapper.removeChild(wrapper.querySelector('.info-container'));
-  }
-  // append the new information container to the wrapper
-  wrapper.appendChild(infoContainer);
+  // make the body that will hold all the weather information and append it to the body so it displays on screen
+  makeBody(weatherInfo);
+
   // change spaces to '-' because the unsplash api separates search terms with a '-'
   userInput = userInput.replace('+', '-');
   // call background.js function to make api call to get the weather at that location
--- src/weather.js
+++ src/weather.js
@@ -6,11 +6,19 @@ async function getWeatherInfo(location) {
   });
   const jsonInfo = await info.json();
   const kelvinTemp = jsonInfo.main.temp;
+  const kelvinLowest = jsonInfo.main.temp_min;
+  const kelvinHighest = jsonInfo.main.temp_max;
+  console.log(jsonInfo);
   // build an object with all the necessary weather information and return it
   return {
     locationName: jsonInfo.name,
-    celsiusTemp: kelvinTemp - 273.15,
-    fahrenheitTemp: 1.8 * (kelvinTemp - 273.15) + 32,
+    celsiusTemp: Math.trunc(kelvinTemp - 273.15).toString().concat('°'),
+    fahrenheitTemp: Math.trunc(1.8 * (kelvinTemp - 273.15) + 32).toString().concat('°'),
+    celsiusLowest: Math.trunc(kelvinLowest - 273.15).toString().concat('°'),
+    fahrenheitLowest: Math.trunc(1.8 * (kelvinLowest - 273.15) + 32).toString().concat('°'),
+    celsiusHighest: Math.trunc(kelvinHighest - 273.15).toString().concat('°'),
+    fahrenheitHighest: Math.trunc(1.8 * (kelvinHighest - 273.15) + 32).toString().concat('°'),
+    description: jsonInfo.weather[0].description,
   };
 }
 
